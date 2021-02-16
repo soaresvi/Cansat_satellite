934deb3b9a4312a4abc9d3dffaa1c5307b883371
soaresvi <61430270+soaresvi@users.noreply.github.com>
2021-02-16 23:28:02 +0100

    Update sensor_bom280.ino

--- measurement_sensors/sensor_bom280.ino
+++ measurement_sensors/sensor_bom280.ino
@@ -1,1 +1,42 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
 
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
+
+Adafruit_BMP280 bmp;
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!bmp.begin()) {
+    Serial.println(F("bpm280 wiring error"));
+    while (1);
+  }
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
+                  Adafruit_BMP280::SAMPLING_X2,
+                  Adafruit_BMP280::SAMPLING_X16,
+                  Adafruit_BMP280::FILTER_X16,
+                  Adafruit_BMP280::STANDBY_MS_500);
+}
+
+void loop() {
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25));
+    Serial.println(" m");
+
+    Serial.println();
+    delay(2000);
+}
